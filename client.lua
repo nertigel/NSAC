8b1c436a6af1157ee20fbc551eaa9869f48e8925
Patrik Nertigel <nertigel14475@gmail.com>
2020-04-27 20:55:02 +0300

    Add files via upload

--- client.lua
+++ client.lua
@@ -8,7 +8,7 @@ print = function(trash)
 end
 
 --[[
-	NSAC Main
+	NSAC Main Loop
 ]]
 Citizen.CreateThread(function()
 	while true do
@@ -25,14 +25,34 @@ Citizen.CreateThread(function()
 		if weaponDamageModifier > 1.0 then
 			TriggerServerEvent('nsac:trigger', 'nsac_3 - damage multiplier ('..weaponDamageModifier..')')
 		end
+
+		if Config.invincibilityCheck and not IsEntityVisible(GetPlayerPed(-1)) then
+			TriggerServerEvent('nsac:trigger', 'nsac_4 - invincibility')
+		end
 	end
 end)
+
+--[[
+	NSAC Secondary Loop(against modifiers)
+]]
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
 		SetPedInfiniteAmmoClip(PlayerPedId(), false)
+		SetPlayerInvincible(PlayerId(), false)
+		SetEntityInvincible(PlayerPedId(), false)
+		SetEntityCanBeDamaged(PlayerPedId(), true)
+		ResetEntityAlpha(PlayerPedId())
 	end
 end)
+
+if Config.currentFramework == 'NONE' then
+	
+elseif Config.currentFramework == 'ESX' then
+	
+elseif Config.currentFramework == 'VRP' then
+
+end
 
 --[[
 	Detection against executors that create resources with a name that contains 16 or more
--- config.lua
+++ config.lua
@@ -5,4 +5,7 @@
 Config = {}
 
 Config.maxHealth = 200 --[[Keep 200 if you didn't change your max health]]
-Config.disallowSpectating = false --[[Set to true if you want users to be punished for spectating]
+Config.disallowSpectating = false --[[Set to true if you want users to be punished for spectating]]
+Config.invincibilityCheck = true
+
+Config.currentFramework = 'NONE' --[[Options: ESX | VRP | NONE]]
